cbcbb27979b97670563d99fa70ee1ccaad10b12b
JHKIMS <wjdgns5131@gmail.com>
2022-10-19 22:49:06 +0900

    JavaScript정리_10_Tab버튼 완성

--- Level2/detail.js
+++ Level2/detail.js
@@ -1,4 +1,19 @@
-document.querySelector('.tab-button').addEventListener('click',function(){
-    document.querySelectorAll('.tab-button').classList.remove('orange');
-    document.querySelector('.tab-button')[0].classList.add("orange");
-});
+let tab = document.querySelectorAll('.tab-button');
+select(0);
+select(1);
+select(2);
+
+function select(tabNum){
+    document.querySelectorAll('.tab-button')[tabNum].addEventListener('click',function(){
+        const orangeRemover = document.querySelectorAll('.tab-button');
+        const showRemover= document.querySelectorAll('.tab-content');
+        for(i=0; i<orangeRemover.length; i++){
+            orangeRemover[i].classList.remove('orange');
+        }
+        for(i=0;i<showRemover.length; i++){
+            showRemover[i].classList.remove('show');
+        }
+        document.querySelectorAll('.tab-button')[tabNum].classList.add('orange');
+        document.querySelectorAll('.tab-content')[tabNum].classList.add('show');
+    });    
+}
