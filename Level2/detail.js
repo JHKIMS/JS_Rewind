47593636834c456d2db6804229605319de20bd23
JHKIMS <wjdgns5131@gmail.com>
2022-10-19 23:57:58 +0900

    JavaScript정리_11_Tab버튼JS수정, EventBubble진행중

--- Level2/detail.js
+++ Level2/detail.js
@@ -1,19 +1,28 @@
-let tab = document.querySelectorAll('.tab-button');
-select(0);
-select(1);
-select(2);
+let tab = document.querySelectorAll(".tab-button");
 
-function select(tabNum){
-    document.querySelectorAll('.tab-button')[tabNum].addEventListener('click',function(){
-        const orangeRemover = document.querySelectorAll('.tab-button');
-        const showRemover= document.querySelectorAll('.tab-content');
-        for(i=0; i<orangeRemover.length; i++){
-            orangeRemover[i].classList.remove('orange');
-        }
-        for(i=0;i<showRemover.length; i++){
-            showRemover[i].classList.remove('show');
-        }
-        document.querySelectorAll('.tab-button')[tabNum].classList.add('orange');
-        document.querySelectorAll('.tab-content')[tabNum].classList.add('show');
-    });    
+document.querySelector('.list').addEventListener('click', function(e){
+    if(e.target === tab[0]){
+        select(0);
+    }else if(e.target === tab[1]){
+        select(1);
+    }else if(e.target === tab[2]){
+        select(2);
+    }
+})
+
+function select(tabNum) {
+  document
+    .querySelectorAll(".tab-button")
+    [tabNum].addEventListener("click", function () {
+      const orangeRemover = document.querySelectorAll(".tab-button");
+      const showRemover = document.querySelectorAll(".tab-content");
+      for (i = 0; i < orangeRemover.length; i++) {
+        orangeRemover[i].classList.remove("orange");
+      }
+      for (i = 0; i < showRemover.length; i++) {
+        showRemover[i].classList.remove("show");
+      }
+      document.querySelectorAll(".tab-button")[tabNum].classList.add("orange");
+      document.querySelectorAll(".tab-content")[tabNum].classList.add("show");
+    });
 }
